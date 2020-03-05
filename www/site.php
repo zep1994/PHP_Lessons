9ce82891b7575f9408c1646c67b948bcb3007038
Thomas Matlock <snaxbags@protonmail.com>
2020-03-05 10:59:44 -0600

    Write first php script

---
+++ www/site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+      //create a variable
+      $characterName = "Thomas";
+      $characterAge = 35;
+
+      //Echo will write out to page
+      echo "<h1>$characterName's site</h1>";
+      echo "<p>Here is his age: $characterAge";
+     ?>
+
+
+</body>
+</html>
