2870f6f27c7bd99b0bb3f72d0f63e0714729ebed
Thomas Matlock <snaxbags@protonmail.com>
2020-03-05 13:52:20 -0600

    String manipulation

--- www/site.php
+++ www/site.php
@@ -14,9 +14,19 @@
 
       //Echo will write out to page
       echo "<h1>$characterName's site</h1>";
-      echo "<p>Here is his age: $characterAge";
+      //can change variable in the middle of script
+      $characterAge = 5;
+      echo "<p>Here is his age: $characterAge<br />";
+
+      //DATA TYPES
+      $phrase = "";
+
+      //MANIPULATE STRING
+      $phrase = "Tom is a man who says a lot of things";
+      // echo strtolower($phrase);
+      // echo strlen("Tom is a man who s");
+      echo substr($phrase, 17);
+
      ?>
-
-
 </body>
 </html>
