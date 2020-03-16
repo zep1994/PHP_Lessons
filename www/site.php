f68950157157fbdbf7bf2c9a4a47f9a483725401
Thomas Matlock <snaxbags@protonmail.com>
2020-03-16 13:33:20 -0500

    Update site.php

--- www/site.php
+++ www/site.php
@@ -1,28 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+  if ($argc !== 2) {
+    
+    echo "Usage: php hello.php <name>.\n";
+    exit(1);
 
-  <!-- //CREATE A FORM -->
-  <form action="site.php" method="get">
-     Adjective: <input type="text" name="adj1" />
-     Noun: <input type="text" name="noun" />
-     Plural Noun: <input type="text" name="pnoun" />
-     Person in Room: <input type="text" name="pir" />
-     Adjective: <input type="text" name="adj2" />
-     <p><input type="submit" /></p>
-  </form>
+  }
+  
+  //writing out the date in PHP 
+  //given raw data
+  $raw_date = '22. 11. 1968';
 
-    <?php
+  //create the start date
+  $start = DateTime::createFromFormat('d. m. Y', $raw_date);
 
-      
+  //return the date in the format specified
+  echo 'Start date: ' . $start->format('Y-m-d') . "\n";
 
-     ?>
+  $name = $argv[1];
+  echo "Hello, $name\n";
 
+  // create a copy of $start and add one month and 6 days
+  $end = clone $start;
 
-</body>
-</html>
+  $end->add(new DateInterval('P1M6D'));
+
+  $diff = $end->diff($start);
+
+  echo 'Difference: ' . $diff->format('%m month, %d days (total: %a days)') . "\n";
+
+  // Difference: 1 month, 6 days (total: 37 days)
+
+  $periodInterval = DateInterval::createFromDateString('first thursday');
+  $periodIterator = new DatePeriod($start, $periodInterval, $end, DatePeriod::EXCLUDE_START_DATE);
+
+  //loop through each date
+  foreach ($periodIterator as $date) {
+
+    //output each date in the period
+
+    echo $date -> format('Y-m-d') . ' ';
+  }
+
+?>
